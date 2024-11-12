28f691947f6af6be4a1922059e993849a7eeff90
tickymaster <81648159+tickymaster@users.noreply.github.com>
2024-11-12 22:56:11 +0100

    Update 0.01

---
+++ Euler.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using Tonysmath;
+class Euler
+{
+    public static void Main()
+    {
+        Eulersnum(2000, 15); // accuracy and power of the number
+    }
+    public static void Eulersnum(int accuracy, int power)
+    {
+        BigInteger e = 0;
+        for (int n = 0; n < accuracy; n++)
+        {
+            //Console.WriteLine((Tonysmath.power(power, n)) + "/" + Tonysmath.factorial(n));
+            e += StandardOperations.Power(power, n)/ StandardOperations.Factorial(n);
+            Console.WriteLine("Iteration: " + n + ", Euler's number: " + e);
+        }
+        Console.WriteLine("Euler's number raised to the power of: " + power + " is: " + e);
+    }
+}
+    
---
+++ Tonysmath.cs
@@ -0,0 +1,116 @@
+﻿using System.Numerics;
+namespace Tonysmath
+{
+    class StandardOperations
+    {
+        public static BigInteger Power(BigInteger x, BigInteger y)
+        {
+            if (x == 0 && y == 0) return 0;
+            if (y == 0) return 1;
+            BigInteger initial = x;
+            for (int i = 1; i < y; i++)
+            {
+                x *= initial;
+            }
+            return x;
+        }
+        public static BigInteger Factorial(BigInteger n)
+        {
+            if (n < 0) throw new ArgumentException("Factorial of a negative number is undefined.");
+            if (n == 0)
+            {
+                return 1;
+            }
+            BigInteger k = n;
+            for (int i = 1; i < k; i++)
+            {
+                n = n * (k - i);
+            }
+            return n;
+        }
+    }
+    class MatrixOperations
+    {
+        public static int[,] Multiply(int[,] m1, int[,] m2)
+        {
+            if (m1.GetLength(1) != m2.GetLength(0))
+            {
+                throw new ArgumentException("Dimension error exception. Dimension of the matricies dont match.");
+            }
+            int[,] result = new int[m1.GetLength(0), m2.GetLength(1)];
+            int sum;
+            for (int i = 0; i < m1.GetLength(0); i++)
+            {
+                for (int j = 0; j < m2.GetLength(0); j++)
+                {
+                    sum = 0;
+                    for (int k = 0; k < m2.GetLength(1); k++)
+                    {
+                        sum += m1[i, k] * m2[k, j];
+                    }
+                    result[i, j] = sum;
+                }
+            }
+            return result;
+        }
+        public static int[,] Add(int[,] m1, int[,] m2)
+        {
+            if (m1.GetLength(0) != m2.GetLength(0) || m1.GetLength(1) != m2.GetLength(1))
+            {
+                throw new ArgumentException("Dimension error exception. Dimension of the matricies dont match.");
+            }
+            int[,] result = new int[m1.GetLength(0), m1.GetLength(1)];
+            for (int i = 0; i < m1.GetLength(0); i++)
+            {
+                for (int j = 0; j < m1.GetLength(1); j++)
+                {
+                    result[i, j] = m1[i, j] + m2[i, j];
+                }
+            }
+            return result;
+        }
+        public static int[,] Subtract(int[,] m1, int[,] m2)
+        {
+            if (m1.GetLength(0) != m2.GetLength(0) || m1.GetLength(1) != m2.GetLength(1))
+            {
+                throw new ArgumentException("Dimension error exception. Dimension of the matricies dont match.");
+            }
+            int[,] result = new int[m1.GetLength(0), m1.GetLength(1)];
+            for (int i = 0; i < m1.GetLength(0); i++)
+            {
+                for (int j = 0; j < m1.GetLength(1); j++)
+                {
+                    result[i, j] = m1[i, j] - m2[i, j];
+                }
+            }
+            return result;
+        }
+        public static int[,] Divide(int[,] m1, int[,] m2)
+        {
+            if (m1.GetLength(1) != m2.GetLength(0))
+            {
+                throw new ArgumentException("Dimension error exception. Dimension of the matricies dont match.");
+            }
+            int[,] result = new int[m1.GetLength(0), m2.GetLength(1)];
+            int sum;
+            for (int i = 0; i < m1.GetLength(0); i++)
+            {
+                for (int j = 0; j < m2.GetLength(0); j++)
+                {
+                    sum = 0;
+                    for (int k = 0; k < m2.GetLength(1); k++)
+                    {
+                        sum += m1[i, k] * (1 / m2[k, j]);
+                    }
+                    result[i, j] = sum;
+                }
+            }
+            return result;
+        }
+        public static int[,] PlaceHolder(int[,] m1, int[,] m2)
+        {
+            int[,] result = new int[m1.GetLength(0), m2.GetLength(1)];
+            return result;
+        }
+    }
+}
